e32caa3b036e90855ac8d3a2a0d86b4263dd96e3
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-03-23 00:02:50 +0100

    Add tab for another part of the project

--- MIW-P1/Form1.Designer.cs
+++ MIW-P1/Form1.Designer.cs
@@ -45,6 +45,11 @@ namespace MIW_P1
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -57,7 +62,7 @@ namespace MIW_P1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(558, 38);
+            this.button1.Location = new System.Drawing.Point(554, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(25, 23);
             this.button1.TabIndex = 0;
@@ -67,7 +72,7 @@ namespace MIW_P1
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(559, 159);
+            this.button2.Location = new System.Drawing.Point(555, 144);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(24, 23);
             this.button2.TabIndex = 1;
@@ -77,7 +82,7 @@ namespace MIW_P1
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Location = new System.Drawing.Point(8, 23);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(540, 23);
             this.textBox1.TabIndex = 2;
@@ -86,7 +91,7 @@ namespace MIW_P1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Location = new System.Drawing.Point(9, 5);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 15);
             this.label1.TabIndex = 3;
@@ -94,7 +99,7 @@ namespace MIW_P1
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 159);
+            this.textBox2.Location = new System.Drawing.Point(8, 144);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(540, 23);
             this.textBox2.TabIndex = 4;
@@ -102,7 +107,7 @@ namespace MIW_P1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 132);
+            this.label2.Location = new System.Drawing.Point(8, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 15);
             this.label2.TabIndex = 5;
@@ -111,7 +116,7 @@ namespace MIW_P1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Location = new System.Drawing.Point(8, 60);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(84, 15);
             this.label3.TabIndex = 6;
@@ -119,7 +124,7 @@ namespace MIW_P1
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(114, 72);
+            this.textBox3.Location = new System.Drawing.Point(110, 57);
             this.textBox3.MaxLength = 5;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(39, 23);
@@ -128,7 +133,7 @@ namespace MIW_P1
             // button3
             // 
             this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(411, 75);
+            this.button3.Location = new System.Drawing.Point(407, 60);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(141, 23);
             this.button3.TabIndex = 8;
@@ -139,7 +144,7 @@ namespace MIW_P1
             // button4
             // 
             this.button4.Enabled = false;
-            this.button4.Location = new System.Drawing.Point(213, 199);
+            this.button4.Location = new System.Drawing.Point(209, 184);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(183, 23);
             this.button4.TabIndex = 9;
@@ -149,7 +154,7 @@ namespace MIW_P1
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(13, 242);
+            this.textBox4.Location = new System.Drawing.Point(9, 227);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
@@ -160,7 +165,7 @@ namespace MIW_P1
             // button5
             // 
             this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button5.Location = new System.Drawing.Point(223, 75);
+            this.button5.Location = new System.Drawing.Point(219, 60);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(141, 23);
             this.button5.TabIndex = 11;
@@ -171,7 +176,7 @@ namespace MIW_P1
             // button6
             // 
             this.button6.Enabled = false;
-            this.button6.Location = new System.Drawing.Point(223, 114);
+            this.button6.Location = new System.Drawing.Point(219, 99);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(141, 23);
             this.button6.TabIndex = 12;
@@ -182,7 +187,7 @@ namespace MIW_P1
             // button7
             // 
             this.button7.Enabled = false;
-            this.button7.Location = new System.Drawing.Point(411, 113);
+            this.button7.Location = new System.Drawing.Point(407, 98);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(141, 23);
             this.button7.TabIndex = 13;
@@ -190,31 +195,64 @@ namespace MIW_P1
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(598, 563);
+            this.tabControl1.TabIndex = 14;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(590, 535);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Part I";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(590, 535);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Part II";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(595, 562);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "MIW-P1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -236,6 +274,9 @@ namespace MIW_P1
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
